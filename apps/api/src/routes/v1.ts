f8e36b7154ac1693cd1bce910585225e1fbf5342
Dominik Keller <mail@dominikkeller.com>
2026-04-15 13:26:07 +0200

    chore: expose only /v1/map, remove scrape/crawl surface
    - Trim v1 routes to /map + health endpoints only
    - Delete v1 scrape/crawl/crawl-status/crawl-cancel/scrape-status controllers
    - Delete orphaned v0 scrape/crawl/crawlPreview/keyAuth/status controllers
    - Remove queue-worker.ts (nothing enqueues jobs anymore)
    - Drop workers/worker:production npm scripts
    - docker-compose: remove worker and puppeteer-service containers

--- apps/api/src/routes/v1.ts
+++ apps/api/src/routes/v1.ts
@@ -1,14 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Response } from "express";
 import { RateLimiterMode } from "../types";
 import { authenticateUser } from "../controllers/auth";
-import { crawlController } from "../controllers/v1/crawl";
-import { scrapeController } from "../../src/controllers/v1/scrape";
-import { crawlStatusController } from "../controllers/v1/crawl-status";
 import { mapController } from "../controllers/v1/map";
 import { RequestWithMaybeAuth } from "../controllers/v1/types";
-import { createIdempotencyKey } from "../services/idempotency/create";
-import { crawlCancelController } from "../controllers/v1/crawl-cancel";
-import { scrapeStatusController } from "../controllers/v1/scrape-status";
 import { livenessController } from "../controllers/v1/liveness";
 import { readinessController } from "../controllers/v1/readiness";
 import expressWs from "express-ws";
@@ -36,22 +30,9 @@ export function authMiddleware(
   };
 }
 
-function idempotencyMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  (async () => {
-    if (req.headers["x-idempotency-key"]) {
-      createIdempotencyKey(req);
-    }
-    next();
-  })().catch((err) => next(err));
-}
-
 function wrap(
-  controller: (req: Request, res: Response) => Promise<any>
-): (req: Request, res: Response, next: NextFunction) => any {
+  controller: (req: express.Request, res: Response) => Promise<any>
+): (req: express.Request, res: Response, next: NextFunction) => any {
   return (req, res, next) => {
     controller(req, res).catch((err) => next(err));
   };
@@ -61,34 +42,7 @@ expressWs(express());
 
 export const v1Router = express.Router();
 
-v1Router.post(
-  "/scrape",
-  authMiddleware(RateLimiterMode.Scrape),
-  wrap(scrapeController)
-);
-
-v1Router.post(
-  "/crawl",
-  authMiddleware(RateLimiterMode.Crawl),
-  idempotencyMiddleware,
-  wrap(crawlController)
-);
-
 v1Router.post("/map", authMiddleware(RateLimiterMode.Map), wrap(mapController));
-
-v1Router.get(
-  "/crawl/:jobId",
-  authMiddleware(RateLimiterMode.CrawlStatus),
-  wrap(crawlStatusController)
-);
-
-v1Router.get("/scrape/:jobId", wrap(scrapeStatusController));
-
-v1Router.delete(
-  "/crawl/:jobId",
-  authMiddleware(RateLimiterMode.Crawl),
-  crawlCancelController
-);
 
 // Health/Probe routes
 v1Router.get("/health/liveness", livenessController);
